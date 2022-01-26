9bdb8e78b5fdfd0637761bac268129c821ffab87
Victor Hugo Sánchez <hugo71119@gmail.com>
2022-01-26 09:58:14 -0600

    cuarto commit, terminando triangulo y todo circulo

--- figura.js
+++ figura.js
@@ -15,5 +15,33 @@ console.group('Triángulos');
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const ladoTriangulo3 = 4;
+const alturaTriangulo = 5.5;
 console.log(`Los lados del triangulo miden: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm y ${ladoTriangulo3}cm`);
+console.log(`La altura del triángulo es de: ${alturaTriangulo}cm`);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTriangulo3;
+console.log(`El perimetro del triángulo es de: ${perimetroTriangulo}cm`);
+
+const areaTriangulo = ((ladoTriangulo3 * alturaTriangulo) / 2);
+console.log(`El área del triangulo es de: ${areaTriangulo}cm^2`);
+console.groupEnd();
+
+
+//Código del circulo
+console.group('Circulos');
+//Rádio
+const radio = 4;
+console.log(`El radio del circulo es: ${radio}cm`);
+//Diámetro
+const diametro = radio * 2;
+console.log(`El diametro del circulo es: ${diametro}cm`);
+//PI
+const PI = Math.PI;
+console.log(`Pi es igual a: ${PI}`);
+//Circunferencia
+const perimetroCirculo = diametro * PI;
+console.log(`El perimetro del circulo es: ${perimetroCirculo}`);
+//Área
+const areaCirculo = (radio * radio) * PI;
+console.log(`El área del circulo es: ${areaCirculo}`);
 console.groupEnd();

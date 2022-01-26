054f75028ddf62623594be45f5c629b7107e7c16
Victor Hugo Sánchez <hugo71119@gmail.com>
2022-01-25 22:58:52 -0600

    tercer commit, area y perimetro de cuadrado y triangulo

---
+++ figura.js
@@ -0,0 +1,19 @@
+//Código del cuadrado
+console.group('Cuadrados');
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El perímetro del cuadrado miden: ${perimetroCuadrado}cm`);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log(`El perímetro del cuadrado miden: ${areaCuadrado}cm^2`);
+console.groupEnd();
+
+//Código del triangulo
+console.group('Triángulos');
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo3 = 4;
+console.log(`Los lados del triangulo miden: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm y ${ladoTriangulo3}cm`);
+console.groupEnd();
